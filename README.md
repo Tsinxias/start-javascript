2ea80e5a71a7ba31f1335dd5b181539b74bd8ade
Zhang Tsinxias <zhangtsinxias@gmail.com>
2018-01-07 17:54:11 +0100

    devinette activity1

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # start-javascript
 
-### Activity :  
+### Activity 1:  
+Create a riddle. A random number is chosen.  
+6 tries are given to the player.  
+
+### Activity 2:  
 Build a program to manage user's contacts through Javascript.  
 John Smith and Jane Doe are the contact per default inside the program.  
 The program should be able to list contacts and add new ones. When finished, quit.  
--- activity_1/js/devinette.js
+++ activity_1/js/devinette.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Activité : jeu de devinette
 */
 
@@ -11,6 +11,24 @@ console.log("Bienvenue dans ce jeu de devinette !");
 var solution = Math.floor(Math.random() * 100) + 1;
 
 // Décommentez temporairement cette ligne pour mieux vérifier le programme
-//console.log("(La solution est " + solution + ")");
+// console.log("(La solution est " + solution + ")");
 
 // TODO : complétez le programme
+
+for (var i=1; i <= 6; i++) {
+  var number = Number(prompt('Trouvez le nombre entre ' + 1 + ' et ' +  100 + ' compris.'));
+  if (number <= 0 || number > 100) {
+    console.log(number + ' est en dehors de l\'intervalle 1-100');
+  } else if (number > solution) {
+    console.log(number + ' est trop grand');
+  } else if (number < solution) {
+    console.log(number + ' est trop petit');
+  } else if (number === solution) {
+    console.log('Bravo ! La solution était ' + solution);
+    console.log('Vous avez trouvé en ' + i + ' essai(s)');
+    break;
+  }
+  if (number !== solution && i === 6) {
+    console.log('Perdu... La solution était ' + solution);
+  }
+}
